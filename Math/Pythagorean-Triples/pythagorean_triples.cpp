4ffe896437e6298476ce4f3d47f3be1f83010568
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-08-23 22:10:03 +0530

    C++ solution

--- Math/Pythagorean-Triples/pythagorean_triples.cpp
+++ Math/Pythagorean-Triples/pythagorean_triples.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#define LL long long
 
+using namespace std;
+
+int main() {
+	LL n;
+	cin >> n;
+	if (n == 1 || n == 2)
+	    cout << -1;
+	else {
+	    LL a, b;
+	    if (n & 1)
+	        a = (n * n + 1) / 2, b = (n * n - 1) / 2;
+	    else
+	        a = (n * n + 4) / 4, b = (n * n - 4) / 4;
+	    cout << a << ' ' << b;
+	}
+	return 0;
+}
