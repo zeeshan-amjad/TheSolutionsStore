9c03e958264c84b892a923dbdf02f38a92031643
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-08-30 03:26:19 +0530

    Update CHEFCRUN.py

--- Algorithms/Dynamic-Programming/CHEFCRUN/CHEFCRUN.py
+++ Algorithms/Dynamic-Programming/CHEFCRUN/CHEFCRUN.py
@@ -1,28 +1,28 @@
-    def get_sum (a, l, r):
-        s = 0
-        for i in range (l, r):
-            s += a[i]
-        return s
+def get_sum (a, l, r):
+    s = 0
+    for i in range (l, r):
+        s += a[i]
+    return s
+ 
+def max_sum (a, l, r):
+    s = 0
+    ans = 0
+    for i in range (l, r):
+        s += a[i]
+        if s < 0:
+            s = 0
+        ans = max (ans, s)
+    return ans
      
-    def max_sum (a, l, r):
-        s = 0
-        ans = 0
-        for i in range (l, r):
-            s += a[i]
-            if s < 0:
-                s = 0
-            ans = max (ans, s)
-        return ans
-     
-    for _ in range (int (raw_input())):
-        n = int (raw_input())
-        a = map (int, raw_input().split())
-        l, r = map (int, raw_input().split())
+for _ in range (int (raw_input())):
+    n = int (raw_input())
+    a = map (int, raw_input().split())
+    l, r = map (int, raw_input().split())
+    
+    l -= 1
+    r -= 1
         
-        l -= 1
-        r -= 1
+    x = get_sum (a, l, r) + 2 * get_sum (a, r - n, l) - 2 * max_sum (a, r - n, l)
+    y = get_sum (a, r - n, l) + 2 * get_sum (a, l, r) - 2 * max_sum (a, l, r)
         
-        x = get_sum (a, l, r) + 2 * get_sum (a, r - n, l) - 2 * max_sum (a, r - n, l)
-        y = get_sum (a, r - n, l) + 2 * get_sum (a, l, r) - 2 * max_sum (a, l, r)
-        
-        print min (x, y) 
+    print min (x, y) 
