3fefe3bddb77f00324be01f6aa10170605ec1f2b
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-01 00:25:38 +0530

    Create UVa11150.cpp

---
+++ Ad-hoc/UVa-11150/UVa11150.cpp
@@ -0,0 +1,23 @@
+//You're reading zeeshan_amjad's code
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio (0), cin.tie (0)
+#define MOD 1e9 + 7
+#define ll long long
+#define ld long double
+#define FOR(i, a, b) for(int i = a; i < b; ++i)
+#define rFOR(i, a, b) for(int i = a; i > b; --i)
+#define PAIR pair <int, int>
+#define PQ priority_queue <ld>
+#define MAP map <int, int>
+#define pb push_back
+
+using namespace std;
+
+int main () {
+    int n;
+    while (cin >> n)
+        cout << n + n / 2 << '\n';
+    return 0;
+}
