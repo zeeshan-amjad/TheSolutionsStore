892a22a41202486fc94896b190af219866a9630c
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-12-29 10:04:43 +0530

    Update test.cpp

--- Strings/SPOJ PERIOD/test.cpp
+++ Strings/SPOJ PERIOD/test.cpp
@@ -5,12 +5,10 @@
 using namespace std;
 
 int F[MAX], n;
-bool match[MAX];
 string s;
 
 void build_failure_function (string pattern) {
     memset (F, 0, sizeof (F));
-    memset (match, 0, sizeof (match));
     F[0] = F[1] = 0;
     for (int i = 2; i <= pattern.size(); ++i) {
         int j = F[i - 1];
@@ -18,33 +16,13 @@ void build_failure_function (string pattern) {
             if (pattern[i - 1] == pattern[j]) {
                 F[i] = j + 1;
                 break;
-            }
-            
+            }            
             if (!j) {
                 F[i] = 0;
                 break;
             }
-            
             j = F[j];
         }
-    }
-}
-
-void kmp (string text, string pattern) {
-    build_failure_function (pattern);
-    int i = 0, j = 0;
-    while (j < text.size()) {
-        if (text[j] == pattern[i]) {
-            i++, j++;
-            if (i == pattern.size())
-                match[j - 1] = 1, i = F[i];
-        }
-        
-        else if (i > 0)
-            i = F[i];
-            
-        else
-            j++;
     }
 }
 
